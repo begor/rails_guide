a74d757bc891f0ed7ce308e326eb67e70200c146
Biryukov Egor <birykovegor@gmail.com>
2016-11-29 18:54:06 +0300

    Tests for uniqueness

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161129150515) do
+ActiveRecord::Schema.define(version: 20161129154616) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
