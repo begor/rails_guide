f8f68ea86d10fbf85cbc2cacd417ee53da850cc5
Biryukov Egor <birykovegor@gmail.com>
2016-11-29 21:18:04 +0300

    Signup form and tests for it

--- test/integration/user_signup_test.rb
+++ test/integration/user_signup_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class UserSignupTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not create user" do
+    get signup_path
+    assert_select 'form[action="/signup"]'
+    assert_no_difference 'User.count' do
+      post signup_path, params: { user: { name:  "",
+                                         email: "user@invalid",
+                                         password: "foo",
+                                         password_confirmation: "bar" } }
+    end
+    assert_template 'users/new' # Re-renders
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors', count: 8
+  end
 end
