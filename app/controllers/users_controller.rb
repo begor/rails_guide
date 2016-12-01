ae5df8f17da0c95f52ed534389de80880277e158
Biryukov Egor <birykovegor@gmail.com>
2016-12-01 15:35:26 +0300

    Failed edits

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,16 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = USer.find(params[:id])
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+    else
+      render 'edit'
+    end
   end
 
   private
