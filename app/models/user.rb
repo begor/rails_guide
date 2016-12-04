c61fcc6fbdebde5b0968a9fc0dd5d3f80226c87e
Egor Biriukov <birykovegor@gmail.com>
2016-12-04 18:27:46 +0300

    User attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
+  before_save   :downcase_email
+  before_create :create_activation_digest
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 
@@ -8,8 +10,6 @@ class User < ApplicationRecord
             format: {with: VALID_EMAIL_REGEX},
             uniqueness: { case_sensitive: false })
   validates(:password, presence: true, length: {minimum: 6}, allow_nil: true)
-
-  before_save { email.downcase! }
 
   has_secure_password
 
@@ -36,8 +36,18 @@ class User < ApplicationRecord
     BCrypt::Password.create(string, cost: cost)
   end
 
-  # Remember token
   def self.new_token
     SecureRandom.urlsafe_base64
   end
+
+  private
+
+    def downcase_email
+      email.downcase!
+    end
+
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
